ebfc9498e6a66baca6a8c9cbdab74e8999abae9c
Luke go <sangch.go@gmail.com>
2022-04-21 16:34:51 +0900

    ODROID: pwm: Changed basic polarity setting to normal.
    Change-Id: Ie46d758d5a901c69c308ad661dd0bd210cd3ddd9
    Signed-off-by: Luke go <sangch.go@gmail.com>

--- drivers/pwm/pwm-rockchip.c
+++ drivers/pwm/pwm-rockchip.c
@@ -105,7 +105,7 @@ static void rockchip_pwm_get_state(struct pwm_chip *chip,
 
 	if (pc->data->supports_polarity) {
 		if (!(val & PWM_DUTY_POSITIVE))
-			state->polarity = PWM_POLARITY_INVERSED;
+			state->polarity = PWM_POLARITY_NORMAL;
 	}
 
 	clk_disable(pc->pclk);
