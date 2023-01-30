dd6f94b2e3ccc1f68142d92857f5d51eb3757d62
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2023-01-30 13:48:40 +0200

    serial: 8250_dma: Fix DMA Rx completion race
    commit 31352811e13dc2313f101b890fd4b1ce760b5fe7 upstream.
    
    __dma_rx_complete() is called from two places:
      - Through the DMA completion callback dma_rx_complete()
      - From serial8250_rx_dma_flush() after IIR_RLSI or IIR_RX_TIMEOUT
    The former does not hold port's lock during __dma_rx_complete() which
    allows these two to race and potentially insert the same data twice.
    
    Extend port's lock coverage in dma_rx_complete() to prevent the race
    and check if the DMA Rx is still pending completion before calling
    into __dma_rx_complete().
    
    Reported-by: Gilles BULOZ <gilles.buloz@kontron.com>
    Tested-by: Gilles BULOZ <gilles.buloz@kontron.com>
    Fixes: 9ee4b83e51f7 ("serial: 8250: Add support for dmaengine")
    Cc: stable@vger.kernel.org
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20230130114841.25749-2-ilpo.jarvinen@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: I156ff787171c426fcea49fc55d37ef8d2c64cb29

--- drivers/tty/serial/8250/8250_dma.c
+++ drivers/tty/serial/8250/8250_dma.c
@@ -103,6 +103,17 @@ static void __dma_rx_complete(void *param)
 	tty_flip_buffer_push(tty_port);
 }
 
+static void dma_rx_complete(void *param)
+{
+	struct uart_8250_port *p = param;
+	struct uart_8250_dma *dma = p->dma;
+	unsigned long flags;
+
+	spin_lock_irqsave(&p->port.lock, flags);
+	if (dma->rx_running)
+		__dma_rx_complete(p);
+	spin_unlock_irqrestore(&p->port.lock, flags);
+}
 #endif
 
 int serial8250_tx_dma(struct uart_8250_port *p)
@@ -236,7 +247,11 @@ int serial8250_rx_dma(struct uart_8250_port *p)
 		return -EBUSY;
 
 	dma->rx_running = 1;
+#ifdef CONFIG_ARCH_ROCKCHIP
 	desc->callback = __dma_rx_complete;
+#else
+	desc->callback = dma_rx_complete;
+#endif
 	desc->callback_param = p;
 
 	dma->rx_cookie = dmaengine_submit(desc);
